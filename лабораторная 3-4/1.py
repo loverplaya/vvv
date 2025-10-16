9f83d58b70f466c405b2245974b1613cf0489226
loverplaya <maxim.bryzgin@yandex.ru>
2025-10-16 08:23:15 +0300

    Add files via upload

---
+++ лабораторная 3-4/1.py
@@ -0,0 +1,6 @@
+a,b,h = int(input()),int(input()),int(input())
+x=range(a,b,h)
+for i in x:
+    print(i)
+    y = 3*i*2+5*i/13
+    print(y)
---
+++ лабораторная 3-4/4.pas
@@ -0,0 +1,24 @@
+﻿program four;
+var rost: array[1..15] of integer;
+fam: array[1..15] of string = ('Иванов','Петров','Сидоров','Стулов','Столов','Смирнов','Кактусов','Ручков','Мышков','Таков','Ковров','Фогов','Сладость','Гадость','Друнов');
+i,maxRost,index : integer;
+begin
+  for i := 1 to 15 do 
+    rost[i] := Random(150, 200);
+  for i := 1 to 15 do
+    writeln(fam[i],' - ', rost[i], ';');
+  
+maxRost := rost[1];
+  for i := 2 to 15 do
+  if maxRost < rost[i] then
+    maxRost := rost[i];
+maxRost := rost[1];
+index := 1;
+for i := 2 to 15 do
+if rost[i] > maxRost then
+  begin
+  maxRost := rost[i];
+  index := i;
+  end;
+writeln('Самый высокий: ',fam[index],' - ',maxRost);
+end.
