c1d2d28bfa3d6e192a369c04b557995480ed7c98
loverplaya <maxim.bryzgin@yandex.ru>
2025-10-16 08:22:11 +0300

    Delete лаба 2 directory

--- лаба 2/4.py
+++
@@ -1,9 +0,0 @@
-import random
-
-rost = [random.randint(155,200) for i in range(15)]
-familia = ["Столов","Стулов","Стенов","Полов","Листков","Наушников","Телефонов","Иванов","Петров","Друнов","Ч","Фог","Сладсоть","Гадость","Друнность"]
-sred = sum(rost)/len(rost)
-max_rost = max(rost)
-samiy = rost.index(max_rost)
-print(samiy)
-print("Средний рост",sred, "Самый высокий",max_rost,familia[samiy])
